ae60ce6ed4c40908cf69c6e88c08815fc47c8aaf
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-02-28 15:33:15 +0800

    Initial commit

---
+++ app/src/main/kotlin/com/cogitator/customuiviews/RecyclerviewActivity.kt
@@ -0,0 +1,11 @@
+package com.cogitator.customuiviews
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar on 28/02/2019
+ */
+class RecyclerviewActivity : AppCompatActivity() {
+
+
+}
