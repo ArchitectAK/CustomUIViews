1781d9fc47bc66c636f4e0b633341e4d3fd9e80d
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-03-01 13:55:06 +0800

    refactored

--- app/src/main/kotlin/com/cogitator/customuiviews/RecyclerviewActivity.kt
+++ app/src/main/kotlin/com/cogitator/customuiviews/RecyclerviewActivity.kt
@@ -1,11 +1,16 @@
 package com.cogitator.customuiviews
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * @author Ankit Kumar on 28/02/2019
  */
-class RecyclerviewActivity : AppCompatActivity() {
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
 
 }
