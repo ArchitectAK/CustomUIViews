dcba6d4a9f3a12bb445fcc81df6e9bd7e1c1b61a
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2019-03-05 18:10:18 +0800

    Delete RecyclerViewActivityy.kt

--- app/src/main/kotlin/com/cogitator/customuiviews/RecyclerViewActivityy.kt
+++
@@ -1,9 +0,0 @@
-package com.cogitator.customuiviews
-
-import android.support.v7.app.AppCompatActivity
-
-/**
- * @author Ankit Kumar on 05/03/2019
- */
-class RecyclerViewActivityy : AppCompatActivity() {
-}
